7556010ddc7539ac470e30e0948f835c748dc532
Nafiul Alam Miraj <nafiul.miraj@strativ.se>
2025-06-04 16:18:58 +0600

    fixed filter

--- api/v1/views.py
+++ api/v1/views.py
@@ -2,8 +2,10 @@ from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from apps.filterApp.models import Member
 from .serializers import MemberSerializer
+from .filters import MemberFilter
 
 class MemberViewSet(viewsets.ModelViewSet):
     queryset = Member.objects.all()
     serializer_class= MemberSerializer
     filter_backends =[DjangoFilterBackend]
+    filterset_class = MemberFilter
